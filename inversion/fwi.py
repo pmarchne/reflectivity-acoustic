6bed899458946275b9d543d3e18ceaf13db2cbcc
pmarchne <philippe.marchner23@gmail.com>
2026-06-01 09:58:30 +0200

    add fwi MAP multiple inversions

--- inversion/fwi.py
+++ inversion/fwi.py
@@ -1,105 +1,88 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.optimize import minimize
-from src.config import Config
-from src.layers import create_layers_from_interfaces, update_layer_slice
-from src.simulation import Simulation
-from src.misfit import l2_misfit
+from sampling.synthetic_experiment import prepare_synthetic_model
+from src.utilities import timer
 
-
-def make_fwi_objective(d_obs, layers, sim, cost_history):
-    """Define FWI objective function"""
-
-    def fwi_objective(vp_vec):
-        # Build model
-        lay = update_layer_slice(layers, vp_slice=vp_vec, start=1)
-        # Forward
-        d_cal, cache = sim.forward(lay)
-        # Misfit
-        residual = d_cal - d_obs
-        phi = l2_misfit(d_cal, d_obs)
+def make_map_objective(bayes, cost_history):
+    """Define Regularized FWI objective function for MAP estimation"""
+    def map_objective(model):
+        #with timer("likelihood eval"):
+        phi_data = bayes.log_likelihood(model)
+        phi_prior = bayes.log_prior(model)
+        phi_map = phi_data + phi_prior
 
         # Gradient via adjoint
-        grad_vp, _ = sim.gradient(
-            residual=residual[0],
-            layers=lay,
-            cache=cache,
-        )
-        grad_phi_vp = grad_vp[1:]  # first layer gradient is set to 0
+        #with timer("grad likelihood eval"):
+        grad_phi_data = bayes.grad_log_likelihood(model) 
+        grad_phi_prior = bayes.grad_log_prior(model)
+        grad_phi_map = grad_phi_data + grad_phi_prior
+
+        l2_misfit = bayes.l2_misfit(model)
+        print("l2 misfit = ", l2_misfit)
         # Store cost
-        cost_history.append(phi)
-        vp_str = "[" + ", ".join(f"{v:7.1f}" for v in vp_vec) + "]"
-        print(f"Iter {len(cost_history):>2} | misfit: {phi:.4e} | Vp: {vp_str}")
+        cost_history.append(phi_map)
+        m_str = "[" + ", ".join(f"{m:7.1f}" for m in model) + "]"
+        print(f"Iter {len(cost_history):>2} | MAP: {phi_map:.4e} (Data: {phi_data:.2e}, Prior: {phi_prior:.2e}) | m: {m_str}")
 
-        return phi, grad_phi_vp
+        return -phi_map, -grad_phi_map # because we maximize the MAP, so minimize -MAP
 
-    return fwi_objective
+    return map_objective
+
+def fwi_scipy():
+    seed=42
+    
+    #m_ref = np.array([2000.0, 2200.0, 4400.0, 1900.0, 3800.0, 2900.0, 3500.0])
+    #path = "FD_comparison/data/seis_v2_nofs"
+    #bayes_model, param = prepare_fd_model(file_path=path, seed=seed)
+    
+
+    m_ref = np.array([1800.0, 3500.0, 150.]) # reference model
+    bayes_model, param = prepare_synthetic_model(seed=seed)
+    print("beta =", bayes_model.beta)
 
 
-def fwi_scipy(plot_history):
-    # Build experiment
-    config = Config(
-        n_receivers=16,
-        x_min=0.0,
-        x_max=700.0,
-        z_rec=75.0,
-        z_src=50.0,
-        nq_prop=512,
-        nq_evan=128,
-        f0=10.0,
-        total_time=1.024,
-        delay=0.2,
-        source_deriv=True,
-        epsilon=1.5,
-        free_surface=True,
-    )
-    sim = Simulation(config)
+    vp_bounds_list = [param.vp_bounds] * param.n_vp
+    h_bounds_list = [param.h_bounds] * param.n_h
+    all_bounds = vp_bounds_list + h_bounds_list
 
-    # true model
-    z_interfaces = np.array([0.0, 100.0, 250.0, 400.0, 700.0])
-    # try this for cycle-skipping failure
-    # vp_true = np.array([1500.0, 3000.0, 1800.0, 3500.0])
-    vp_true = np.array([1500.0, 3000.0, 2800.0, 3500.0])
-    print(f"Target Vp: {vp_true[1:]}")
-    rho = np.array([1200.0, 2100.0, 2000.0, 2200.0])
-    layers = create_layers_from_interfaces(z_interfaces, vp_true, rho)
+    phi_data = bayes_model.log_likelihood(m_ref)
+    grad_phi_data = bayes_model.grad_log_likelihood(m_ref)
+    print(phi_data)
+    print("\n")
+    print(grad_phi_data)
+    unique_map_minima = []
+    num_starts = 15
 
-    # Observed data
-    d_obs, _ = sim.forward(layers)
-    vp_init = np.array([3500.0, 4000.0, 4000.0])
-
-    methods = ["L-BFGS-B", "TNC", "SLSQP"]
-    results_history = {}
-
-    for method in methods:
-        print(f"Starting FWI ({method})\n{'-'*30}")
+    for i in range(num_starts):
+        # Generate a random initial guess
+        random_m_init = param.draw_from_prior()
+        print("Initial model :", random_m_init)
+        # random_m_init = np.array([1800.0, 3500.0, 140.])
+        print(f"--- Start {i+1}/{num_starts} ---")
         cost_history = []
-        obj_func = make_fwi_objective(d_obs, layers, sim, cost_history)
+        obj_func = make_map_objective(bayes_model, cost_history)
         res = minimize(
             obj_func,
-            vp_init,
-            method=method,
+            random_m_init,
+            method="L-BFGS-B",
             jac=True,
-            bounds=[(500.0, 7000.0)] * len(vp_init),
-            options={"maxiter": 50}
+            bounds=all_bounds,
+            options={"maxiter": 100, "ftol": 1e-6, "gtol": 1e-4}
         )
         print(f"{'-'*50}\nInverted Vp: {res.x}")
-        results_history[method] = cost_history
-        print(f"{method} completed in {len(cost_history)} evaluations.")
-
-    # Plotting the Cost History
-    if plot_history:
-        plt.figure(figsize=(6, 3))
-        for method, history in results_history.items():
-            plt.semilogy(history, label=method, marker='o', markersize=4)
-
-        plt.title("FWI Convergence")
-        plt.xlabel("Iterations")
-        plt.ylabel("L2 Misfit")
-        plt.grid(True, which="both", ls="-", alpha=0.5)
-        plt.legend()
-        plt.show()
-
+        print(f"Completed in {len(cost_history)} evaluations.")
+        
+        if res.success:
+            unique_map_minima.append({'vp': res.x, 'map_val': -res.fun})
+            print(f"minimum found!")
+        else:
+            print(f"did not converged")
+            
+    print("\n=== All Discovered MAP Local Minima ===")
+    # Sort them by objective value so the best candidate is at the top
+    unique_map_minima = sorted(unique_map_minima, key=lambda x: x['map_val'])
+    for idx, minimum in enumerate(unique_map_minima):
+        print(f"MAP Mode #{idx}: Objective = {minimum['map_val']:.4e} | Vp = {minimum['vp'].astype(int)}")
 
 if __name__ == "__main__":
-    fwi_scipy(plot_history=True)
+    fwi_scipy()
---
+++ inversion/fwi_simple.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+from src.config import Config
+from src.layers import create_layers_from_interfaces, update_layer_slice
+from src.simulation import Simulation
+from src.misfit import l2_misfit
+
+
+def make_fwi_objective(d_obs, layers, sim, cost_history):
+    """Define FWI objective function"""
+
+    def fwi_objective(vp_vec):
+        # Build model
+        lay = update_layer_slice(layers, vp_slice=vp_vec, startv=1)
+        # Forward
+        d_cal = sim.forward(lay)
+        # Misfit
+        residual = d_cal - d_obs
+        phi = l2_misfit(d_cal, d_obs)
+
+        # Gradient via adjoint
+        grad_vp, _, _ = sim.gradient(
+            residual=residual[0],
+            layers=lay
+        )
+        grad_phi_vp = grad_vp[1:]  # first layer gradient is set to 0
+        # Store cost
+        cost_history.append(phi)
+        vp_str = "[" + ", ".join(f"{v:7.1f}" for v in vp_vec) + "]"
+        print(f"Iter {len(cost_history):>2} | misfit: {phi:.4e} | Vp: {vp_str}")
+
+        return phi, grad_phi_vp
+
+    return fwi_objective
+
+
+def fwi_scipy(plot_history):
+    # Build experiment
+    config = Config(
+        n_receivers=16,
+        x_min=0.0,
+        x_max=700.0,
+        z_rec=75.0,
+        z_src=50.0,
+        nq_prop=512,
+        nq_evan=128,
+        f0=10.0,
+        total_time=1.024,
+        delay=0.2,
+        source_deriv=True,
+        epsilon=1.5,
+        free_surface=True,
+    )
+    sim = Simulation(config)
+
+    # true model
+    z_interfaces = np.array([0.0, 100.0, 250.0, 400.0, 700.0])
+    # try this for cycle-skipping failure
+    # vp_true = np.array([1500.0, 3000.0, 1800.0, 3500.0])
+    vp_true = np.array([1500.0, 3000.0, 2800.0, 3500.0])
+    print(f"Target Vp: {vp_true[1:]}")
+    rho = np.array([1200.0, 2100.0, 2000.0, 2200.0])
+    layers = create_layers_from_interfaces(z_interfaces, vp_true, rho)
+
+    # Observed data
+    d_obs = sim.forward(layers)
+    vp_init = np.array([3500.0, 4000.0, 4000.0])
+
+    methods = ["L-BFGS-B", "TNC", "SLSQP"]
+    results_history = {}
+
+    for method in methods:
+        print(f"Starting FWI ({method})\n{'-'*30}")
+        cost_history = []
+        obj_func = make_fwi_objective(d_obs, layers, sim, cost_history)
+        res = minimize(
+            obj_func,
+            vp_init,
+            method=method,
+            jac=True,
+            bounds=[(500.0, 7000.0)] * len(vp_init),
+            options={"maxiter": 50}
+        )
+        print(f"{'-'*50}\nInverted Vp: {res.x}")
+        results_history[method] = cost_history
+        print(f"{method} completed in {len(cost_history)} evaluations.")
+
+    # Plotting the Cost History
+    if plot_history:
+        plt.figure(figsize=(6, 3))
+        for method, history in results_history.items():
+            plt.semilogy(history, label=method, marker='o', markersize=4)
+
+        plt.title("FWI Convergence")
+        plt.xlabel("Iterations")
+        plt.ylabel("L2 Misfit")
+        plt.grid(True, which="both", ls="-", alpha=0.5)
+        plt.legend()
+        plt.show()
+
+
+if __name__ == "__main__":
+    fwi_scipy(plot_history=True)
